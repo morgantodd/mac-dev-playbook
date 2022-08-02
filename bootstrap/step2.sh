33f597869c75c179b728fbe4e8758b86e0dd4361
Daniel Koo <1916351+danielkoo@users.noreply.github.com>
2022-08-02 12:59:06 +1000

    Update step2.sh
    added steps to set up the path to homebrew

--- bootstrap/step2.sh
+++ bootstrap/step2.sh
@@ -7,4 +7,7 @@ then
   echo "installing homebrew"
   MY_DIR=$(dirname $0)
   /bin/bash -c "./${MY_DIR}/brew-installer.sh"
+  echo "Setting up the path to brew"
+  echo 'eval "$(/opt/homebrew/bin/barew shellenv)"' >> ~/.bashrc
+  eval "$(/opt/homebrew/bin/barew shellenv)"
 fi
