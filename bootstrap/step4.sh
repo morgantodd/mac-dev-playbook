972de59cb0e8c3ed4679e0b67c1b35dc008ccc69
Daniel Koo <1916351+danielkoo@users.noreply.github.com>
2022-08-02 13:07:57 +1000

    Update step4.sh
    install jq via asdf for more flexibility in controlling the version

--- bootstrap/step4.sh
+++ bootstrap/step4.sh
@@ -9,3 +9,13 @@ then
   asdf install python 3.8.13 # the mac-dev-playbook wants 3.8, but could test 3.9+ in the future
   asdf global python 3.8.13
 fi
+
+
+echo "setting up jq"
+
+if ! asdf plugin list | grep '^jq$' > /dev/null
+then
+  asdf plugin add jq
+  asdf install jq latest
+  asdf global jq latest
+fi
