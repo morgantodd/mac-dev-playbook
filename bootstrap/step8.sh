b0dad33f92ba7c8b83bbab215d948a3daf3ebc34
Daniel Koo <1916351+danielkoo@users.noreply.github.com>
2022-08-02 17:21:46 +1000

    added a hack to symlink the .bash dir, as the dotfiles_files ansible module does not handle dirs

--- bootstrap/step8.sh
+++ bootstrap/step8.sh
@@ -1,13 +1,15 @@
 
+ln -s ~/code/github/personal/dotfiles/.bash ~/
+
 # generate ssh key so I can clone from chuck
 # brew install cntlm
 # copy cntlm.conf from OneDrive
-export http_proxy=http://localhost:3128
-export https_proxy=http://localhost:3128
-export HTTP_PROXY=http://localhost:3128
-export HTTPS_PROXY=http://localhost:3128
-git config --global http.proxy http://localhost:3128
-git config --global https.proxy http://localhost:3128
+#export http_proxy=http://localhost:3128
+#export https_proxy=http://localhost:3128
+#export HTTP_PROXY=http://localhost:3128
+#export HTTPS_PROXY=http://localhost:3128
+#git config --global http.proxy http://localhost:3128
+#git config --global https.proxy http://localhost:3128
 
 
 asdf plugin add vault
@@ -31,14 +33,11 @@ asdf global snyk latest
 
 ####################
 # to build devlabsctl on the mac
+asdf plugin add golang
 asdf install golang 1.16
 asdf global golang 1.16
 go get -u github.com/go-bindata/go-bindata/...
 go install github.com/jstemmer/go-junit-report@latest
-go get  github.com/optiopay/klar
-
-# IAG download klar
-https://github.com/optiopay/klar/releases
 
 # get a nice git prompt in bash:
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
