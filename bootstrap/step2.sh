69b3dee7de46039c133dd44da051db640e3e4bef
Daniel Koo <1916351+danielkoo@users.noreply.github.com>
2022-08-03 21:48:33 +1000

    support different brew installation paths which are based on the architecture

--- bootstrap/step2.sh
+++ bootstrap/step2.sh
@@ -2,12 +2,21 @@
 
 set -euo pipefail
 
+if [[ $(arch) == "i386" ]]; then
+    BREW_PATH=/usr/local/bin/brew
+elif [[ $(arch) == "arm64" ]]; then
+    BREW_PATH=/opt/homebrew/bin/brew
+else
+    echo "Unknown architecture $(arch); only i386 and arm64 are supported"
+    exit 1
+fi
+
 if ! which brew > /dev/null
 then
   echo "installing homebrew"
   MY_DIR=$(dirname $0)
   /bin/bash -c "./${MY_DIR}/brew-installer.sh"
   echo "Setting up the path to brew"
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.bashrc
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+  echo 'eval "$(${BREW_PATH} shellenv)"' >> ~/.bashrc
+  eval "$(${BREW_PATH} shellenv)"
 fi
